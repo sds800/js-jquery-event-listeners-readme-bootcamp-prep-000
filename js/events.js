0c667cf71dee7a2957a69d404ea6e32a5e32b07a
SDS 800 <jeremyhyman@gmail.com>
2018-01-31 18:39:12 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,21 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!')
+  })
+}
 
+function frameIt() {
+
+}
+
+function pressIt(){
+
+}
+
+function submitIt() {
+
+}
 $(document).ready(function(){
 
 // call functions here
