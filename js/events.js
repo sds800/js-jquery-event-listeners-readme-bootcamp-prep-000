e076a922894cc02de06cc4450c1e6a96a6e227c4
SDS 800 <jeremyhyman@gmail.com>
2018-01-31 19:53:00 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -6,8 +6,8 @@ function getIt() {
 }
 
 function frameIt() {
-  .on('load', function () {
-    $('img').addClass('tasty')
+  $('img').on('load', function () {
+    $('img').addClass('.tasty')
 
 })
 }
